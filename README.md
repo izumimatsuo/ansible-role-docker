a6d0f4cf1518e9ccc0933632116db730d6dbc4b0
いずみ <izumi@Matsuo-MacBook-Air.local>
2020-07-11 19:06:01 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ansible-role-docker
+
+CentOS 7 に docker, docker-compose を構築する ansible role です。
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,17 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_docker_is_installed(host):
+    package = host.package("docker-ce")
+    assert package.is_installed
+    assert package.version.startswith("19.0")
+
+
+def test_docker_compose_is_installed(host):
+    assert '1.26' in host.check_output('docker-compose --version')
